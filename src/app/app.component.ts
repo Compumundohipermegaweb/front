83e0f3634eca54fcaba93172f293f7a869c800ea
Damian Lisas <damian.lisas@gmail.com>
2021-05-03 22:17:32 -0300

    Fixeados tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { PingService } from "./service/ping/ping.service"
-import { PingResponse } from "./service/ping/ping.service"
 
 @Component({
   selector: 'app-root',
@@ -10,16 +8,7 @@ import { PingResponse } from "./service/ping/ping.service"
 export class AppComponent {
 
   title = 'hefesto-front';
-  apiVersion: String = "";
 
-  constructor(private pingService: PingService) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.pingApi()
-  }
-
-  pingApi() {
-    this.pingService.pingApi()
-        .subscribe((data: PingResponse) => this.apiVersion = data.version );
-  }
 }
--- src/app/service/ping/ping.service.spec.ts
+++ src/app/service/ping/ping.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PingService } from './ping.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpHandler } from "@angular/common/http"
 
 describe('PingService', () => {
   let service: PingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({providers: [HttpClient, HttpHandler]});
     service = TestBed.inject(PingService);
   });
 
