7e2164f852785233d55b7ec684d3de8cd1022d5c
Damian Lisas <damian.lisas@gmail.com>
2021-05-10 00:50:00 -0300

    Agregada vista de factura. CU-4n6jke

--- src/app/sale-invoice/sale-invoice.component.ts
+++ src/app/sale-invoice/sale-invoice.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import { Item } from '../sales/sales.component';
 import { SaleResponse } from '../sales/service/sales.service';
 
 @Component({
@@ -8,14 +12,22 @@ import { SaleResponse } from '../sales/service/sales.service';
 })
 export class SaleInvoiceComponent implements OnInit {
 
+  displayedColumns: string[];
+  dataSource = new MatTableDataSource<Item>();
+  totalCost: number;
+
   saleResponse: SaleResponse
 
-  constructor() {}
+  constructor(private changeDetectorRefs: ChangeDetectorRef) {
+    this.displayedColumns = ["quantity", "detail", "price", "subTotal"]
+    
+  }
 
   ngOnInit(): void {
     if(history.state != null && history.state["data"] != null) {
       this.saleResponse = history.state["data"]
     }
+    this.dataSource.data = this.saleResponse.detail
+    this.totalCost = this.saleResponse.detail.map((item: Item) => item.quantity * item.price).reduce((a, b) => a + b)
   }
-
 }
